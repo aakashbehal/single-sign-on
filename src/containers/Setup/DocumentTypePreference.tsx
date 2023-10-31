85097bc67b1fdd54df94c463ec5dab80db5c2dd3
aakash behal <akashhe@gmail.com>
2023-10-31 20:44:59 +0530

    EQDM-1573 done

--- src/containers/Setup/DocumentTypePreference.tsx
+++ src/containers/Setup/DocumentTypePreference.tsx
@@ -315,7 +315,7 @@ const AddEditClient = ({ onHide, show, data, dispatch, productTypes }: any) => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    {!data ? 'Add Document Group' : 'Edit Document Group'}
+                    {!data ? 'Add Document Type Preference' : 'Edit Document Type Preference'}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body className="show-grid">
