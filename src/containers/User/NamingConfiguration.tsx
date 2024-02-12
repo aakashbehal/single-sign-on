26404ae1828220937de1d12f70917ac40698b823
aakash behal <akashhe@gmail.com>
2024-02-12 17:36:46 +0530

    reverted

--- src/containers/User/NamingConfiguration.tsx
+++ src/containers/User/NamingConfiguration.tsx
@@ -636,7 +636,7 @@ const NamingConfiguration = () => {
                                                     <Form.Control
                                                         as="select"
                                                         name="productType"
-                                                        value={defaultDocGroupConfigCode}
+                                                        value={defaultProductCode}
                                                         onChange={(e) => setDefaultProductCode(e.target.value)}
                                                         className="select_custom white">
                                                         <option disabled value="" selected>Select Product Type...</option>
@@ -657,7 +657,7 @@ const NamingConfiguration = () => {
                                                 <Form.Group as={Col} className="mb-2">
                                                     <Form.Control
                                                         as="select"
-                                                        value={defaultDocTypeCode}
+                                                        value={defaultDocumentType}
                                                         onChange={(e) => setDefaultDocumentType(e.target.value)}
                                                         name="documentType"
                                                         className="select_custom white">
